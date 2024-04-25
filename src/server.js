263ae135e894af9cf52788c5f7b59bd58471f6f3
DidierDev672 <ariasdev672@gmail.com>
2024-04-24 21:42:10 -0500

    Configuración de imagen de docker

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const apiRoutes = require('./interfaces/apiRoutes');
 const logger = require('./utils/logger');
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/', apiRoutes);
+app.use(morgan("dev"));
 
 app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
